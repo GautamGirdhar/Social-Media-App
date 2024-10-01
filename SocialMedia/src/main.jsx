1ff93cc0420b6f4664a50b82b4a7339644a9f5b2
GautamGirdhar <girdhargautam6@gmail.com>
2024-10-01 19:53:45 +0530

    first commit

---
+++ SocialMedia/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./routes/App.jsx";
+import CreatePost, { createPostAction } from "./components/CreatePost.jsx";
+import PostList, { postLoader } from "./components/PostList.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList />, loader: postLoader },
+      {
+        path: "/create-post",
+        element: <CreatePost />,
+        action: createPostAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
